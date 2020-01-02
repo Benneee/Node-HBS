a04c15bc252cdf62d21bb8a58f43e0016b79c5f1
benneee <benedictiknkeonye@gmail.com>
2020-01-02 15:28:45 +0100

    Using the weather endpoint

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
-console.log('Client side javascript loaded!');
+const log = console.log;
+const con = console;
+log("Client side javascript loaded!");
+
+const address = "Lagos";
+const geocodeUrl = `http://localhost:3000/weather?address=${address}`;
+
+fetch(geocodeUrl).then(response => {
+  response
+    .json()
+    .then(data => {
+      if (data.error) {
+        log(data.error);
+      } else {
+        log(data.location);
+        log(data.forecast);
+      }
+    })
+    .catch(error => log(error));
+});
